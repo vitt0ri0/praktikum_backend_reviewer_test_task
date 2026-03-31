dac7cf1f2f142cf803a297f0b9576c7cc01aa40d
vitt0ri0 <vitt0ri0.progr@gmail.com>
2026-03-31 21:04:48 +0300

    uv formatting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Комментарии по стилю: Посмотри пожалуйста требования к коду (https://docs.google.com/document/d/1s_FqVkqOASwXK0DkOJZj5RzOm4iWBO5voc_8kenxXbw/edit?tab=t.0) и перед коммитом стоит использовать uv / black / isort одну из утилит для упрощения себе жизни
-# - Уточню некоторые несоблюденные пункты: констистентность кавычек, бэкслеши, отступы. 
+# - Уточню некоторые несоблюденные пункты: констистентность кавычек, бэкслеши, отступы.
 # - Не хватает docstrings для методов
 
 
@@ -30,7 +30,7 @@ class Calculator:
         today_stats = 0
 
         # здесь происходит что-то странное, и похоже обращаемся не к экземпляру класса, а к самому классу Record, нужно проверить и поправить
-        for Record in self.records: 
+        for Record in self.records:
             if Record.date == dt.datetime.now().date():
                 today_stats = today_stats + Record.amount
         return today_stats
@@ -49,20 +49,20 @@ class CaloriesCalculator(Calculator):
         x = self.limit - self.get_today_stats()
         if x > 0:
             return (
-                # строки взаимодействия с клиентов я бы вынес в константы. В продакшене это обычно на стороне фронтенда. Но если рабоатем с апи, то эти коснтанты часто в БД, и их может менять админ продукта. 
+                # строки взаимодействия с клиентов я бы вынес в константы. В продакшене это обычно на стороне фронтенда. Но если рабоатем с апи, то эти коснтанты часто в БД, и их может менять админ продукта.
                 f"Сегодня можно съесть что-нибудь"
                 f" ещё, но с общей калорийностью не более {x} кКал"
             )
-        else: # лишний else, так как если условие не выполняется, то функция все равно вернет результат
+        else:  # лишний else, так как если условие не выполняется, то функция все равно вернет результат
             return "Хватит есть!"
 
 
 class CashCalculator(Calculator):
-    # Рекомендация: инициалиизровать константы лучше в отдельном классе, т.к. в перспективе их получение 
+    # Рекомендация: инициалиизровать константы лучше в отдельном классе, т.к. в перспективе их получение
     # будет либо из БД, либо из API
-    # Можно назвать метод get_currency_rate и вызывать его внутри get_today_cash_remained, 
-    # тогда не нужно будет передавать эти константы в виде аргументов метода, 
-    # а также не нужно будет их объявлять в виде атрибутов класса, 
+    # Можно назвать метод get_currency_rate и вызывать его внутри get_today_cash_remained,
+    # тогда не нужно будет передавать эти константы в виде аргументов метода,
+    # а также не нужно будет их объявлять в виде атрибутов класса,
     # так как они не будут использоваться нигде кроме этого метода
     USD_RATE = float(60)  # Курс доллар США.
     EURO_RATE = float(70)  # Курс Евро.
@@ -71,17 +71,19 @@ class CashCalculator(Calculator):
         currency_type = currency
         cash_remained = self.limit - self.get_today_stats()
         # логику вычисления можно упростить, и сделать if только для currency, а внутри него уже делать вычисления, тогда не нужно будет дублировать код для каждого типа валюты
-        if currency == "usd":  # стоит сравнивать currency с константой, которые лучше объявить в начале файла, например, CURRENCY_USD = "usd"
+        if (
+            currency == "usd"
+        ):  # стоит сравнивать currency с константой, которые лучше объявить в начале файла, например, CURRENCY_USD = "usd"
             cash_remained /= USD_RATE
             currency_type = "USD"
         elif currency_type == "eur":
             cash_remained /= EURO_RATE
             currency_type = "Euro"
         elif currency_type == "rub":
-            cash_remained == 1.00 # хардкод для рубля? коммента, наверное, не хватает. 
+            cash_remained == 1.00  # хардкод для рубля? коммента, наверное, не хватает.
             currency_type = "руб"
 
-        # опять же, можно делать if (...): return (...) без else. 
+        # опять же, можно делать if (...): return (...) без else.
         if cash_remained > 0:
             return f"На сегодня осталось {round(cash_remained, 2)} {currency_type}"
         elif cash_remained == 0:
@@ -95,16 +97,19 @@ class CashCalculator(Calculator):
     def get_week_stats(self):
         super().get_week_stats()
 
+
 if __name__ == "__main__":
     # создадим калькулятор денег с дневным лимитом 1000
     cash_calculator = CashCalculator(1000)
-            
-    # дата в параметрах не указана, 
+
+    # дата в параметрах не указана,
     # так что по умолчанию к записи должна автоматически добавиться сегодняшняя дата
-    cash_calculator.add_record(Record(amount=145, comment="кофе")) 
+    cash_calculator.add_record(Record(amount=145, comment="кофе"))
     # и к этой записи тоже дата должна добавиться автоматически
     cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
     # а тут пользователь указал дату, сохраняем её
-    cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
-                    
+    cash_calculator.add_record(
+        Record(amount=3000, comment="бар в Танин др", date="08.11.2019")
+    )
+
     print(cash_calculator.get_today_cash_remained("rub"))
